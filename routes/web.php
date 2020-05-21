32480f27aecbac76c1dfcb75e831340fa0d15f7b
Zilola <62849764+zry77@users.noreply.github.com>
2020-05-21 19:09:56 +0500

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/questionnaires', 'QuestionnaireController@store');
+Route::get('/questionnaires/create', 'QuestionnaireController@create');
+Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
+
+Route::post('/questionnaires/{questionnaire}/questions', 'QuestionController@store');
+Route::get('/questionnaires/{questionnaire}/questions/create', 'QuestionController@create');
+Route::delete('/questionnaires/{questionnaire}/questions/{question}', 'QuestionController@destroy');
+
+Route::get('/surveys/{questionnaire}-{slug}', 'SurveyController@show');
+Route::post('/surveys/{questionnaire}-{slug}', 'SurveyController@store');
+
