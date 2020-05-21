6278d70463445a58c77933408602fdf1af67da50
Zilola <62849764+zry77@users.noreply.github.com>
2020-05-21 18:53:38 +0500

    Add files via upload

---
+++ app/SurveyResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyResponse extends Model
+{
+    protected $guarded = [];
+
+    public function survey(){
+        return $this->belongsTo(Survey::class);
+    }
+}
